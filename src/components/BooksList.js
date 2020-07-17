d478abcf4a1bfe01ac8a4810b152442c62947c9f
Murilo Roque <muriloengqui@gmail.com>
2020-07-17 05:13:43 -0300

    implement BooksList component

---
+++ src/components/BooksList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../styles/BooksList.scss';
+import PropTypes from 'prop-types';
+
+function BooksList() {
+  return (
+    <table>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      {
+        props.books.map((book, index) => {
+          return (
+            <Book key={index} book={book} />
+          )
+        })
+      }
+    </table>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    books: state.books
+  }
+}
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired  
+}
+
+export default connect(mapStateToProps, null)(BooksList)
