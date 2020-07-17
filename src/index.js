30d47cc4cfecac5f2d9ae352217839842f94da4c
Murilo Roque <muriloengqui@gmail.com>
2020-07-17 05:51:50 -0300

    initialize Redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers/index";
+
+let initialState = {
+  books:[
+    {id: Math.round(100 * Math.random()), title: "Life of Action", category: 'Action'},
+    {id: Math.round(150 * Math.random()), title: 'Becoming', category: 'Biography'},
+    {id: Math.round(200 * Math.random()), title: 'Sapiens: A Brief History of Humankind', category: 'History'},
+    {id: Math.round(250 * Math.random()), title: 'Dracula', category: 'Horror'},
+    {id: Math.round(300 * Math.random()), title: 'The Very Hungry Caterpillar', category: 'Kids'},
+    {id: Math.round(350 * Math.random()), title: 'Microverse', category: 'Learning'},
+    {id: Math.round(400 * Math.random()), title: 'Dune', category: 'Sci-Fi'}
+  ]
+}
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>, 
   document.getElementById('root'),
 );
 
