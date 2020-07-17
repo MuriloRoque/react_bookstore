2e609e22e6a1e905c7a2c93cffc4a571fcfe2f83
Murilo Roque <muriloengqui@gmail.com>
2020-07-17 07:39:34 -0300

    fix linter errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import ACTIONTYPES from '../constants/ActionTypes'
+import ACTIONTYPES from '../constants/ActionTypes';
 
 export const createBook = book => ({
   type: ACTIONTYPES.CREATE_BOOK,
   book,
 });
 
-export const removeBook = (book, index) => ({
+export const removeBook = book => ({
   type: ACTIONTYPES.REMOVE_BOOK,
   book,
 });
--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,8 +1,6 @@
-import { types } from "node-sass"
-
 const ACTIONTYPES = {
   CREATE_BOOK: 'CREATE_BOOK',
-  REMOVE_BOOK: 'REMOVE_BOOK'
-}
+  REMOVE_BOOK: 'REMOVE_BOOK',
+};
 
 export default ACTIONTYPES;
