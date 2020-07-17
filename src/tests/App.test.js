8ab0550f344a71662400eddddf73ba226ac93a9d
Murilo Roque <muriloengqui@gmail.com>
2020-07-17 07:49:05 -0300

    refactor categories constant

--- src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../components/App';
-
-test('renders welcome to bookstore', () => {
-  const { getByText } = render(<App />);
-  const welcomeElement = getByText(/to the bookstore/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
